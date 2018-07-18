9b6c5a6d9783d820288edc3b8af193a57f9b247d
Sean Gregory <sean.gregory@careworkstech.com>
2018-07-18 18:08:04 +0000

    validate that tickets sold are not 0

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -1,4 +1,5 @@
 class Showtime < ApplicationRecord
+  validates :tickets_sold, format: { with: /[1-9]+/, message: "Tickets has to be a number greater than 0"}
   belongs_to :movie
   belongs_to :theater
 end
