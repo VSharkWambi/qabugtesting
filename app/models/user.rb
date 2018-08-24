17f43994272d4caa6ebb7eca74b1e4c5f21c3931
Sean Gregory <sean.gregory@careworkstech.com>
2018-08-24 19:56:54 +0000

    auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@ class User < ApplicationRecord
   def self.find_or_create_by_auth_hash(auth_hash)
     user = User.find_by_uuid(auth_hash["uid"])
     if user.nil?
-      user = User.create!({login: auth_hash['info']['email'], uuid: auth_hash['uid']})
+      user = User.new({login: auth_hash['info']['email'], uuid: auth_hash['uid']})
+      user.skip_pass_validation = true
+      user.save
     end
     user
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: 'email,profile'
+  client_id = '455519824919-j4arbb9un7jdja2k6u7kg33o5dr5eggi.apps.googleusercontent.com'
+  secret = '5lZr8Grl3VJ7lAmYje7mWA5A'
+  provider :google_oauth2, client_id, secret, scope: 'email,profile'
 end
