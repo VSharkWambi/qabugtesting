a0c4add1a9cd0c92f52e7bb6738b0071801ab7b5
Sean.Gregory <sean.gregory@careworkstech.com>
2018-08-24 16:28:06 -0400

    migration and tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
 
   attr_accessor :skip_pass_validation
 
-  has_secure_password
+  has_secure_password validations: false
   validates_presence_of :password, :on => :create, :unless => :skip_pass_validation
 
-  def self.find_or_create_by_auth_hash(auth_hash)
-    user = User.find_by_uuid(auth_hash["uid"])
+  def self.find_or_create_from_auth_hash(auth_hash)
+    user = User.where(uuid: auth_hash["uid"]).first
     if user.nil?
       user = User.new({login: auth_hash['info']['email'], uuid: auth_hash['uid']})
       user.skip_pass_validation = true
